8d2ff73d3c15be5e94c30e758066f90bcedc6de7
kjnh10 <kojinho10@gmail.com>
2017-03-17 21:45:54 +0900

    first commit

---
+++ networkmonitor.py
@@ -0,0 +1,14 @@
+import subprocess, time, datetime
+import re
+
+while True:
+    with open("./netspeedlog.csv", "a") as f:
+        print("started mesuring")
+        output = str(subprocess.check_output(["speedtest"]))
+        downspeed = re.search("[0-9]*\.[0-9]* Mbit/s", output[output.find("Download:"):]).group()
+        upspeed = re.search("[0-9]*\.[0-9]* Mbit/s", output[output.find("Upload:"):]).group()
+        now = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        weekday = datetime.datetime.now().strftime('%a')
+        f.write(now + "," + weekday + "," + downspeed.replace(" ", ",") + "," + upspeed.replace(" ", ",")\n)
+        print("Done")
+        time.sleep(60)
